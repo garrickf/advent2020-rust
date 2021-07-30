1f97f655c95983704922e019a22d608928592b3f
Garrick Fernandez <20538779+garrickf@users.noreply.github.com>
2021-07-30 12:50:57 -0700

    refactor day one and add comments

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -19,9 +19,28 @@ fn read_input_from_file() -> Result<Vec<i32>, io::Error> {
     Ok(nums)
 }
 
-fn main() {
-    let nums = read_input_from_file().expect("Error reading file.");
+// Find the product of two numbers that sum to 2020
+fn part_one(nums: &Vec<i32>) {
+    let mut map = HashMap::new();
 
+    for a in nums {
+        let key = 2020 - a;
+        map.insert(key, a);
+    }
+
+    for b in nums {
+        if map.contains_key(b) {
+            let a = map[b];
+            println!("Found!");
+            println!("{} + {} = 2020", a, b);
+            println!("{} * {} = {}", a, b, a * b);
+            break;
+        }
+    }
+}
+
+// Find the product of three numbers that sum to 2020
+fn part_two(nums: &Vec<i32>) {
     let mut map = HashMap::new();
     for (ai, a) in nums.iter().enumerate() {
         for b in &nums[ai..] {
@@ -31,11 +50,22 @@ fn main() {
         }
     }
 
-    for c in &nums {
+    for c in nums {
         if map.contains_key(c) {
             let (a, b) = map[c];
-            println!("Found {}, {}, {}; product: {}", a, b, c, a * b * c);
+            println!("Found!");
+            println!("{} + {} + {} = 2020", a, b, c);
+            println!("{} * {} * {} = {}", a, b, c, a * b * c);
             break;
         }
     }
+}
+
+fn main() {
+    let nums = read_input_from_file().expect("Error reading file.");
+
+    println!("Part One\n---");
+    part_one(&nums);
+    println!("\nPart Two\n---");
+    part_two(&nums);
 }
